2034933e7ed8185de88a088425101f923d0326a7
Baljeet <baljeet.masih.755@gmail.com>
2023-09-12 17:06:57 +0530

    day first

---
+++ day_first/assets/js/dayone.js
@@ -0,0 +1,37 @@
+// 1 js debugging tool & case senstive
+
+// console.log("hello world one day")
+// // case senstive
+// let kanu = 1;
+// let Kanu = 2;
+// let KNnu = 3;
+// let KANu = 4;
+
+// console.log(KANu)
+
+// 2 alert 
+
+// alert("this page admin can asscess only..")
+
+// 3 get to user decison 
+
+// let a = confirm("Are you working.")
+// if(a){
+//     alert("user agree.")
+// }else{
+//     alert("user not agree.")
+// }
+
+
+// 4 get to user input
+
+// let a = prompt("Please enter your age..")
+// alert(a)
+
+// 5 dom mainuplation 
+
+// console.log(document.body)
+
+document.write("<h1>Kanu</h1>")
+
+console.log("=========================")
